2be7f2a48f54a8cafe833ebd77570946eb7ac47d
frest <frest@DESKTOP-DFPD65U>
2026-02-10 17:09:18 +0200

    Add project files.

---
+++ Regex/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+var regex = new Regex("(-\\d+\\.?\\d?)");
+
+var str = "-42  0  007  123456  -9 DA -0.5  +3.14 -15 -999999 25 -1 13";
+
+foreach (Match match in regex.Matches(str))
+{
+    if (match.Success)
+    {
+        Console.WriteLine(match.Groups[1].Value);
+    }
+}
